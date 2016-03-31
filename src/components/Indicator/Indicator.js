72820cf83db711c9789c8d1c13f923080d4133de
Ron Waldon <jokeyrhyme@gmail.com>
2016-03-31 11:16:44 +1100

    animate dashboard indicator values

--- src/components/Indicator/Indicator.js
+++ src/components/Indicator/Indicator.js
@@ -1,13 +1,25 @@
 import React, { PropTypes } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import Paper from 'material-ui/lib/paper';
 
 import classes from './Indicator.css';
 
+const TRANSITION_PROPS = {
+  transitionEnterTimeout: 1000,
+  transitionLeave: false,
+  transitionName: {
+    enter: classes.valueEnter,
+    enterActive: classes.valueEnterActive
+  }
+};
+
 export const Indicator = ({ children, value }) => (
   <Paper className={classes.self}>
     <label className={classes.title}>{children}</label>
-    <label className={classes.value}>{value}</label>
+    <ReactCSSTransitionGroup {...TRANSITION_PROPS}>
+      <label key={value} className={classes.value}>{value}</label>
+    </ReactCSSTransitionGroup>
   </Paper>
 );
 
