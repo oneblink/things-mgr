efd19618da76a6e0df3247096b5438ef357ea324
Ron Waldon <jokeyrhyme@gmail.com>
2016-06-02 16:01:02 +1000

    TimeAgo component re-renders every 30 seconds

--- src/components/TimeAgo/TimeAgo.js
+++ src/components/TimeAgo/TimeAgo.js
@@ -9,25 +9,41 @@ const MINUTE = 60 * SECOND;
 const HOUR = 60 * MINUTE;
 
 const ELAPSED_MIN = MINUTE;
-const ELAPSED_MAX = HOUR;
+const ELAPSED_MAX = HOUR / 3;
 
-export const TimeAgo = ({ className, value }) => {
-  const date = new Date(value);
-  const elapsed = (new Date()) - date;
-  // clamp elapsed between 1 minute and 1 hour
-  const clampedElapsed = Math.max(Math.min(elapsed, ELAPSED_MAX), ELAPSED_MIN);
-  const timeProps = {
-    className: classnames(classes.self, className),
-    dateTime: value,
-    style: {
-      opacity: Math.max(0.2, 1 - (clampedElapsed / ELAPSED_MAX))
-    }
-  };
+export class TimeAgo extends React.Component {
+  componentDidMount () {
+    // automatically re-render every 30 seconds
+    this.setState({
+      timer: setInterval(() => {
+        this.forceUpdate();
+      }, 30e3)
+    });
+  }
 
-  return (
-    <time {...timeProps}>{timeAgo(date)}</time>
-  );
-};
+  componentWillUnmount () {
+    clearTimeout((this.state || {}).timer);
+  }
+
+  render () {
+    const { className, value } = this.props;
+    const date = new Date(value);
+    const elapsed = (new Date()) - date;
+    // clamp elapsed between 1 minute and 1 hour
+    const clampedElapsed = Math.max(Math.min(elapsed, ELAPSED_MAX), ELAPSED_MIN);
+    const timeProps = {
+      className: classnames(classes.self, className),
+      dateTime: value,
+      style: {
+        opacity: Math.max(0.2, 1 - (clampedElapsed / ELAPSED_MAX))
+      }
+    };
+
+    return (
+      <time {...timeProps}>{timeAgo(date)}</time>
+    );
+  }
+}
 
 TimeAgo.propTypes = {
   className: PropTypes.string,
